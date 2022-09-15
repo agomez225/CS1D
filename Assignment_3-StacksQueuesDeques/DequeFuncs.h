53aa059440d2d94c4e33fc3458da734af9aaa11f
agomez225 <aygomez138@gmail.com>
2022-09-14 20:52:13 -0700

    adding deques

--- Assignment_3-StacksQueuesDeques/DequeFuncs.h
+++ Assignment_3-StacksQueuesDeques/DequeFuncs.h
@@ -34,3 +34,76 @@ class stringDeque
     string getFront();
     void print();
 };
+
+
+
+void stringDeque::insertRear(string data)
+{
+stringDequeNode* temp = new stringDequeNode(data);
+
+    if (rear == NULL)
+    {
+        front = temp;
+        rear = temp;
+    }
+    else
+    {
+        rear->next = temp; // shouldn't there also be a temp->next = rear
+        rear = temp;
+
+
+    }
+
+
+
+}
+
+void stringDeque::deleteRear()
+{
+    if (rear == NULL)
+    return;
+
+    else
+    {
+        stringDequeNode* temp = rear;
+
+        rear = rear->next;
+
+        delete temp;
+
+
+    }
+}
+
+void stringDeque::insertFront(string data)
+{
+
+stringDequeNode* temp = new stringDequeNode(data);
+
+if (front == NULL)
+{
+    front = temp;
+    rear = temp;
+}
+else
+{
+    front->next = temp;
+    front = temp;
+}
+
+}
+
+void stringDeque::deleteFront()
+{
+    if (front == NULL)
+    return;
+
+    else
+    {
+        stringDequeNode* temp = front;
+
+
+    }
+
+}
+
